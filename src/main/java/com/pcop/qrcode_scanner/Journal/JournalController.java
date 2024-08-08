e84bbabf179dded5f310fe95166c5cc4f8c15f2a
steevi <testeven83@gmail.com>
2024-08-08 08:56:05 +0300

    method security updated

--- src/main/java/com/pcop/qrcode_scanner/Journal/JournalController.java
+++ src/main/java/com/pcop/qrcode_scanner/Journal/JournalController.java
@@ -2,6 +2,7 @@ package com.pcop.qrcode_scanner.Journal;
 
 import com.pcop.qrcode_scanner.ExceptionHandler.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,16 +15,19 @@ public class JournalController {
     @Autowired
     private JournalService journalService;
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @GetMapping
     public Iterable<Journal> getAllJournals() {
         return journalService.findAll();
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @GetMapping("/{id}")
     public Journal getJournalById(@PathVariable Long id) {
         return journalService.findById(id).orElseThrow(() -> new ResourceNotFoundException("Journal not found for id: " + id));
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @GetMapping("/{operationDate}")
     public Iterable<Journal> getJournalsByOperationDate(@PathVariable LocalDateTime operationDate) {
         return journalService.findAllByOperationDate(operationDate);
